66d4016578eb8c00a5d1907b8d0937ebc5ca5437
Sebastien <sguimont@hellonetwork.ca>
2023-06-02 13:29:53 -0400

    Ensure to clone the overrideDate instead of just copying it to ensure a fresh value like the default factory.

--- dist/datetime-factory.js
+++ dist/datetime-factory.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.datetimeFactory=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.datetimeFactory = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 module.exports = require('./lib/datetime-factory');
@@ -35,7 +35,10 @@ module.exports.configure = function (newOptions) {
  *  @returns {*} The current date time from the option.factory Function.
  */
 module.exports.now = function () {
-  return overrideDate || options.factory();
+  if(overrideDate) {
+    return overrideDate();
+  }
+  return options.factory();
 };
 
 /**
@@ -44,7 +47,7 @@ module.exports.now = function () {
  * @param date The date to use when asking the current date time.
  */
 module.exports.overwriteDate = function (date) {
-  overrideDate = date;
+  overrideDate = function overrideDate() { return Object.assign({}, date) };
 };
 
 /**
